e1e46cc13a6ebc98550eca9ce6324c8f3c8d0597
R. Colin Kennedy <c@rcolinkennedy.com>
2013-08-06 11:25:36 -0400

    Install Devise, add user, and install Devise views.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Treebook::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  #Installed for Devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
