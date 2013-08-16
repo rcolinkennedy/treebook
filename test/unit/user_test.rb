5cf30203d7e6100b588de154b36850142c9cfc80
R. Colin Kennedy <c@rcolinkennedy.com>
2013-08-16 16:03:23 -0400

    added tests to user model to ensure that user names are correctly formatted

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -28,11 +28,22 @@ class UserTest < ActiveSupport::TestCase
 	end
 
 	test "a user should have a profile name without spaces" do
-		user = User.new
+		user = User.new(first_name: 'Jason', last_name: "Seifer", email: 'jason@teamtreehouse.com')
+		user.password = user.password_confirmation = 'Treeh0use'
+		
 		user.profile_name = "My Profile with Spaces"
 
 		assert !user.save
 		assert !user.errors[:profile_name].empty?
 		assert user.errors[:profile_name].include?("must be formatted correctly.")
 	end
+
+	test "a user can have a correctly formatted profile name" do
+		user = User.new(first_name: 'Jason', last_name: "Seifer", email: 'jason@teamtreehouse.com')
+		user.password = user.password_confirmation = 'Treeh0use'
+
+		user.profile_name = 'jasonseifer1'
+		assert user.valid?
+	end
+
 end
